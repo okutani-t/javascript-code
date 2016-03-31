68d321a66fa38b1a99934ee441364540a5173aaf
okutani <MYACCOUNT@DOMAIN>
2016-03-31 15:04:36 +0900

    add index.html list

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 JavaScriptのテストコードをまとめたやつ
 
-table-total-sumとかはどっかで使えそう。
+table-total-sumとかはどっかで使えそう。→key upの時に発火させた方がいいかも
 
 author: okutani
---
+++ list-data.js
@@ -0,0 +1,68 @@
+var list = [
+  {
+    "dir_name": "chart.js-test",
+    "files": [
+      "bar.html",
+      "doughnut.html",
+      "line.html",
+      "pie.html",
+      "radar.html"
+    ]
+  },
+  {
+    "dir_name": "check-num-only",
+    "files": [
+      "index.html"
+    ]
+  },
+  {
+    "dir_name": "google-maps-api-test",
+    "files": [
+      "index.html",
+      "utf8.html"
+    ]
+  },
+  {
+    "dir_name": "particle-universe",
+    "files": [
+      "index.html"
+    ]
+  },
+  {
+    "dir_name": "start-javascript",
+    "files": [
+      "numgame/index.html",
+      "omikuji/index.html",
+      "slotmachine/index.html",
+      "stoptime/index.html",
+      "stopwatch/index.html",
+      "timejs/index.html",
+      "warikandentaku/index.html"
+    ]
+  },
+  {
+    "dir_name": "stickybox",
+    "files": [
+      "index.html"
+    ]
+  },
+  {
+    "dir_name": "table-total-sum",
+    "files": [
+      "index.html"
+    ]
+  },
+  {
+    "dir_name": "wow-animate",
+    "files": [
+      "index.html"
+    ]
+  },
+  {
+    "dir_name": "yubinbango",
+    "files": [
+      "index.html",
+      "ajaxzip3.html"
+    ]
+  }
+]
