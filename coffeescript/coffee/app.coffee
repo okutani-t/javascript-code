e45bd7668027d131654638e05f6eaec95ae62ed0
okutani-t <okutani.nt@gmail.com>
2017-03-17 01:44:34 +0900

    Add test code for coffeescript

--- coffeescript/coffee/app.coffee
+++ coffeescript/coffee/app.coffee
@@ -1,1 +1,94 @@
-alert "oh my god!"
+###
+ここはコメント
+###
+
+# ここから書くよ
+score = 82
+
+if score > 80
+  console.log 'good!'
+
+console.log "score: #{score}"
+console.log "score: #{score * 10}"
+
+html = """
+  <div id="main">
+    hello
+  </div>
+       """
+
+console.log html
+
+a = [1, 10, 30]
+b = [0..5]
+
+console.log a
+console.log b
+console.log b[1..3]
+
+b[0..2] = ["a", "b", "c"]
+console.log b
+
+obj = name: 'okutani', score: 10
+
+console.log obj
+
+console.log 'ok!' if score > 60
+
+msg = if score > 60 then 'ok.' else 'ng.'
+console.log msg
+
+console.log 'in!' if 5 in [1, 3, 5]
+
+console.log 'name is exist!' if 'name' of obj
+
+switch score
+  when 80
+    console.log '80!'
+  when 81 then console.log '81!'
+  when 82 then console.log '82!'
+  else console.log 'other...'
+
+sum = 0
+sum += i for i in [0..9]
+console.log sum
+
+sum = 0
+for i in [0..9] by 2
+  console.log i
+  sum += i
+console.log sum
+
+# 配列内包
+sum = 0
+total = (sum += i for i in [0..9])
+console.log total
+
+results =
+  okutani: 40
+  yamada: 80
+  sakura: 60
+
+console.log "#{name}: #{score}" for name, score of results
+hello = -> 'hello!'
+console.log hello()
+
+super_hello = (name) -> "#{name}, hello!!!!!!"
+console.log super_hello('okutani')
+
+do -> console.log super_hello('yamada')
+
+class User
+  constructor: (name) ->
+    @name = name
+  greet: -> "hello, #{@name}!"
+
+tom = new User "Tom"
+console.log tom
+console.log tom.greet()
+
+rs = if x? then "found!" else "not found..."
+console.log rs
+
+x = y ? 20
+console.log x
--- coffeescript/js/app.js
+++ coffeescript/js/app.js
@@ -1,1 +1,154 @@
-alert("oh my god!");
+
+/*
+ここはコメント
+ */
+var User, a, b, hello, html, i, j, k, msg, name, obj, ref, results, rs, score, sum, super_hello, tom, total, x;
+
+score = 82;
+
+if (score > 80) {
+  console.log('good!');
+}
+
+console.log("score: " + score);
+
+console.log("score: " + (score * 10));
+
+html = "<div id=\"main\">\n  hello\n</div>";
+
+console.log(html);
+
+a = [1, 10, 30];
+
+b = [0, 1, 2, 3, 4, 5];
+
+console.log(a);
+
+console.log(b);
+
+console.log(b.slice(1, 4));
+
+[].splice.apply(b, [0, 3].concat(ref = ["a", "b", "c"])), ref;
+
+console.log(b);
+
+obj = {
+  name: 'okutani',
+  score: 10
+};
+
+console.log(obj);
+
+if (score > 60) {
+  console.log('ok!');
+}
+
+msg = score > 60 ? 'ok.' : 'ng.';
+
+console.log(msg);
+
+if (5 === 1 || 5 === 3 || 5 === 5) {
+  console.log('in!');
+}
+
+if ('name' in obj) {
+  console.log('name is exist!');
+}
+
+switch (score) {
+  case 80:
+    console.log('80!');
+    break;
+  case 81:
+    console.log('81!');
+    break;
+  case 82:
+    console.log('82!');
+    break;
+  default:
+    console.log('other...');
+}
+
+sum = 0;
+
+for (i = j = 0; j <= 9; i = ++j) {
+  sum += i;
+}
+
+console.log(sum);
+
+sum = 0;
+
+for (i = k = 0; k <= 9; i = k += 2) {
+  console.log(i);
+  sum += i;
+}
+
+console.log(sum);
+
+sum = 0;
+
+total = (function() {
+  var l, results1;
+  results1 = [];
+  for (i = l = 0; l <= 9; i = ++l) {
+    results1.push(sum += i);
+  }
+  return results1;
+})();
+
+console.log(total);
+
+results = {
+  okutani: 40,
+  yamada: 80,
+  sakura: 60
+};
+
+for (name in results) {
+  score = results[name];
+  console.log(name + ": " + score);
+}
+
+hello = function() {
+  return 'hello!';
+};
+
+console.log(hello());
+
+super_hello = function(name) {
+  return name + ", hello!!!!!!";
+};
+
+console.log(super_hello('okutani'));
+
+(function() {
+  return console.log(super_hello('yamada'));
+})();
+
+User = (function() {
+  function User(name) {
+    this.name = name;
+  }
+
+  User.prototype.greet = function() {
+    return "hello, " + this.name + "!";
+  };
+
+  return User;
+
+})();
+
+tom = new User("Tom");
+
+console.log(tom);
+
+console.log(tom.greet());
+
+rs = typeof x !== "undefined" && x !== null ? "found!" : "not found...";
+
+console.log(rs);
+
+x = typeof y !== "undefined" && y !== null ? y : 20;
+
+console.log(x);
