fb3c48dcdc269ebf4e2d54404c81c6dc3f0a3b35
okutani-t <okutani.nt@gmail.com>
2017-07-24 01:12:36 +0900

    Add draft.js

---
+++ react-draftjs-test/readme.md
@@ -0,0 +1,19 @@
+# Set up
+
+* npm install
+
+```
+$ npm install
+```
+
+* build
+
+```
+$ ./node_modules/.bin/webpack
+```
+
+* show dist/index.html
+
+## Author
+
+[okutani](http://okutani.net)
--- react-draftjs-test/src/index.jsx
+++ react-draftjs-test/src/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
+import {Editor, EditorState} from 'draft-js';
 
-class App extends React.Component {
-  render () {
-    return <p> Hello React!</p>;
+class MyEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onChange = (editorState) => this.setState({editorState});
+  }
+  render() {
+    return (
+        <Editor editorState={this.state.editorState} onChange={this.onChange} />
+    );
   }
 }
 
-render(<App/>, document.getElementById('app'));
+ReactDOM.render(
+  <MyEditor />,
+  document.getElementById('container')
+);
