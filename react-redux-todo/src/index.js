25fd3600361d16a4ab9f0a4e2f8459e7eb140741
okutani-t <okutani.nt@gmail.com>
2019-10-16 14:53:35 +0900

    Cosme

--- react-redux-todo/src/index.js
+++ react-redux-todo/src/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import todo from './reducers'
+import todoApp from './reducers'
 import { addTodo } from './actions'
 import App from './components/App'
 
-const store = createStore(todo)
+const store = createStore(todoApp)
 
 store.dispatch(addTodo('Hello React!'))
 store.dispatch(addTodo('Hello Redux!'))
--- react-redux-todo/src/reducers/index.js
+++ react-redux-todo/src/reducers/index.js
@@ -2,4 +2,5 @@ import { combineReducers } from 'redux'
 import todos from './todos'
 
 const todoApp = combineReducers({ todos })
+
 export default todoApp
