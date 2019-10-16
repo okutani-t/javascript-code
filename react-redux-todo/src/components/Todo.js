0ca51f4815dcf2417369e3286b9cb0b813830b1a
okutani-t <okutani.nt@gmail.com>
2019-10-16 17:05:12 +0900

    Add toggleTodo

--- react-redux-todo/src/components/Todo.js
+++ react-redux-todo/src/components/Todo.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Todo = ({ text }) => (
-  <li>
+const Todo = ({ onClick, completed, text }) => (
+  <li
+    onClick={onClick}
+    class={ completed ? 'done' : '' }
+  >
     {text}
   </li>
 )
 
 Todo.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  completed: PropTypes.bool.isRequired,
   text: PropTypes.string.isRequired
 }
 
--- react-redux-todo/src/index.js
+++ react-redux-todo/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import todoApp from './reducers'
-import { addTodo } from './actions'
 import App from './components/App'
 import './index.css'
 
